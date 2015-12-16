432f0a2dafaa670e3be047d003e1d6647c93de39
Nepian <nepia.ms@gmail.com>
2015-12-16 10:08:33 +0900

    Created class "LocationData"

--- src/main/java/com/Nepian/Teleports/Data/LocationData.java
+++ src/main/java/com/Nepian/Teleports/Data/LocationData.java
@@ -1,22 +1,44 @@
 package com.Nepian.Teleports.Data;
 
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LocationData {
-	private Location location;
-	private OfflinePlayer owner;
+import org.bukkit.Location;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import com.Nepian.Teleports.Main;
+import com.Nepian.Teleports.Util.LocationUtil;
+
+public class LocationData implements ConfigurationSerializable {
+	private static final Main plugin = Main.getPlugin();
 	
-	public LocationData(Location location, OfflinePlayer owner) {
+	public static final String PATH_LOCATION;
+	
+	static {
+		PATH_LOCATION = "Location";
+	}
+	
+	private Location location;
+	
+	public LocationData(Location location) {
 		this.location = location;
-		this.owner = owner;
 	}
 	
 	public Location getLocation() {
 		return this.location;
 	}
+
+	@Override
+	public Map<String, Object> serialize() {
+		Map<String, Object> data = new HashMap<String, Object>();
+		
+		return data;
+	}
 	
-	public OfflinePlayer getOwner() {
-		return this.owner;
+	public static LocationData deserialize(Map<String, Object> data) {
+		String stringLoc = (String) data.get(PATH_LOCATION);
+		Location location = LocationUtil.getSimpleLocationFromString(plugin, stringLoc);
+		
+		return new LocationData(location);
 	}
 }
