9ec1054c1c7b400373dbddf2506515c6fcafe3e7
Nepian <nepia.ms@gmail.com>
2015-12-16 10:13:43 +0900

    add member "Name" to class "LocationData"

--- src/main/java/com/Nepian/Teleports/Data/LocationData.java
+++ src/main/java/com/Nepian/Teleports/Data/LocationData.java
@@ -12,16 +12,24 @@ import com.Nepian.Teleports.Util.LocationUtil;
 public class LocationData implements ConfigurationSerializable {
 	private static final Main plugin = Main.getPlugin();
 	
+	public static final String PATH_NAME;
 	public static final String PATH_LOCATION;
 	
 	static {
+		PATH_NAME     = "Name";
 		PATH_LOCATION = "Location";
 	}
 	
+	private String name;
 	private Location location;
 	
-	public LocationData(Location location) {
+	public LocationData(String name, Location location) {
+		this.name = name;
 		this.location = location;
+	}
+	
+	public String getName() {
+		return this.name;
 	}
 	
 	public Location getLocation() {
@@ -32,13 +40,19 @@ public class LocationData implements ConfigurationSerializable {
 	public Map<String, Object> serialize() {
 		Map<String, Object> data = new HashMap<String, Object>();
 		
+		String stringLoc = LocationUtil.toStringSimpleLocation(this.location);
+		
+		data.put(PATH_NAME, this.name);
+		data.put(PATH_LOCATION, stringLoc);
+		
 		return data;
 	}
 	
 	public static LocationData deserialize(Map<String, Object> data) {
+		String name = (String) data.get(PATH_NAME);
 		String stringLoc = (String) data.get(PATH_LOCATION);
 		Location location = LocationUtil.getSimpleLocationFromString(plugin, stringLoc);
 		
-		return new LocationData(location);
+		return new LocationData(name, location);
 	}
 }
