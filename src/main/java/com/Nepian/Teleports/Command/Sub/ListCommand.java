8490a42ac77b900fa88d132d2444071e22e22244
Nepian <nepia.ms@gmail.com>
2015-12-21 15:58:32 +0900

    リストコマンドの追加

---
+++ src/main/java/com/Nepian/Teleports/Command/Sub/ListCommand.java
@@ -0,0 +1,100 @@
+package com.Nepian.Teleports.Command.Sub;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
+
+import com.Nepian.Teleports.TeleportManager;
+import com.Nepian.Teleports.Command.CommandStrings;
+import com.Nepian.Teleports.Command.SubCommand;
+import com.Nepian.Teleports.Data.LocationStringable;
+import com.Nepian.Teleports.Data.TeleportLocationData;
+import com.Nepian.Teleports.Data.TeleportType;
+
+public class ListCommand extends SubCommand {
+	
+	public ListCommand() {
+		super("list");
+		setPermission(CommandStrings.BASE_PERM + "list");
+	}
+
+	@Override
+	public void execute(CommandSender sender, String label, String[] args) throws CommandException {
+		
+		if (args.length == 0) {
+			StringBuffer message = new StringBuffer("");
+			message.append("Teleports End Location List\n");
+			
+			for (TeleportLocationData data : TeleportManager.getEndLocationDatas()) {
+				String location = LocationStringable.toString(data.getBlockLocation());
+				
+				message.append(" # ");
+				message.append("Name: ").append(data.getName()).append(", ");
+				message.append("Location: ").append(location).append("\n");
+			}
+			
+			sender.sendMessage(message.toString());
+			
+			return;
+		}
+		
+		if (args.length == 1) {
+			
+			if (!TeleportManager.hasName(args[0])) {
+				sender.sendMessage("Teleport Location doesn't exist.");
+				return;
+			}
+			
+			TeleportLocationData end = TeleportManager.getEndLocationData(args[0]);
+			String name = end.getName();
+			
+			StringBuilder message = new StringBuilder("");
+			message.append("Teleports Start Location List of [" + name + "]\n");
+			
+			for (TeleportLocationData data :TeleportManager.getTeleportLocationDatas()) {
+				
+				if (data.getType() != TeleportType.START) {
+					continue;
+				}
+				
+				if (!data.getName().equalsIgnoreCase(name)) {
+					continue;
+				}
+				
+				String owner = data.getOwner().getName();
+				String location = LocationStringable.toString(data.getBlockLocation());
+				
+				message.append(" # ");
+				message.append("Owner: ").append(owner).append(", ");
+				message.append("Location: ").append(location).append("\n");
+			}
+			
+			sender.sendMessage(message.toString());
+			
+			return;
+		}
+	}
+
+	@Override
+	public String getPossibleArguments() {
+		return "<EndLocationName>";
+	}
+
+	@Override
+	public int getMinimumArguments() {
+		return 0;
+	}
+
+	@Override
+	public List<String> getTutorial() {
+		return Arrays.asList("Show the list of teleports.");
+	}
+
+	@Override
+	public SubCommandType getType() {
+		return SubCommandType.GENERIC;
+	}
+
+}
